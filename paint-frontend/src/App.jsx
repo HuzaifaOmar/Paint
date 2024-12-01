05862408f0cb0fb3703ca6c58df0aff68bfb000f
Huthaifa <huzaifaomar3@gmail.com>
2024-12-01 11:09:14 +0200

    refactor(frontend): only used useContext to avoid prop drilling

--- paint-frontend/src/App.jsx
+++ paint-frontend/src/App.jsx
@@ -1,51 +1,21 @@
+import React from "react";
 import "./App.css";
+import "./styles/main.css";
+
 import Canvas from "./components/Canvas";
 import Toolbar from "./components/toolbar/Toolbar";
-import React, { useState } from "react";
-import "./styles/main.css";
 import SavePagePopup from "./components/savePopup/SavePagePopup";
+import { DrawingProvider } from "./contexts/DrawingContext";
+
 function App() {
-  const [selectedTool, setSelectedTool] = useState("freehand");
-  const [fillColor, setFillColor] = useState("rgba(240, 240, 240, 1.00)");
-  const [strokeColor, setStrokeColor] = useState("rgba(0, 0, 0, 1.00)");
-  const [lineWidth, setLineWidth] = useState(2.0);
-  const [eraserOn,setEraserOn]=useState(false)
-  const[duplicateTool,setDuplicateTool]=useState(false);
-  const [isPopupOpen, setIsPopupOpen] = useState(false);
   return (
-    <div className="main-screen">
-      <Toolbar
-        selectedTool={selectedTool}
-        setSelectedTool={setSelectedTool}
-        fillColor={fillColor}
-        setFillColor={setFillColor}
-        strokeColor={strokeColor}
-        setStrokeColor={setStrokeColor}
-        lineWidth={lineWidth}
-        setLineWidth={setLineWidth}
-        eraserOn={eraserOn}
-        setEraserOn={setEraserOn}
-        isPopupOpen={isPopupOpen}
-        setIsPopupOpen={setIsPopupOpen}
-        setDuplicateTool={setDuplicateTool}
-      />
-      <SavePagePopup
-                isPopupOpen={isPopupOpen}
-                setIsPopupOpen={setIsPopupOpen}
-            />
-      <Canvas
-        selectedTool={selectedTool}
-        fillColor={fillColor}
-        strokeColor={strokeColor}
-        lineWidth={lineWidth}
-        eraserOn={eraserOn}
-        setFillColor={setFillColor}
-        setStrokeColor={setStrokeColor}
-        setLineWidth={setLineWidth}
-        duplicateTool={duplicateTool}
-        setDuplicateTool={setDuplicateTool}
-      />
-    </div>
+    <DrawingProvider>
+      <div className="main-screen">
+        <Toolbar />
+        <SavePagePopup />
+        <Canvas />
+      </div>
+    </DrawingProvider>
   );
 }
 
